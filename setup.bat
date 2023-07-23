f17df440e4cbf286edd109a34b169d156acdb6cd
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2023-07-23 21:36:37 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,15 @@
+@echo off
 echo Installing all python modules ...
 echo This will take a time ...
+set "modules=tqdm torch torchvision"
+set "python_path=python"
 
+for %%i in (%modules%) do (
+    %python_path% -m pip show %%i > nul 2>&1
+    if not errorlevel 1 (
+        echo %%i is installed.
+    ) else (
+		echo installing %%i
+        pip install %%i
+    )
+)
